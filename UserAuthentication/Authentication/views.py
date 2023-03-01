134e029faa0c5d95c3d39b35acb3988398abb9c9
Md Fahad <fahadbdctglh8@gmail.com>
2023-03-01 21:31:03 +0600

    Create User Model

--- UserAuthentication/Authentication/views.py
+++ UserAuthentication/Authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    diction = {}
+    return render(request, 'Authentication/index.html', context=diction)
+    
--- UserAuthentication/UserAuthentication/settings.py
+++ UserAuthentication/UserAuthentication/settings.py
@@ -16,7 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "Templates")
-
+STATIC_DIR = os.path.join(BASE_DIR, 'Static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'Media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -123,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+
+# MEDIA
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/Media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
